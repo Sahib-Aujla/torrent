eb3a69ff6cfc95ab5d26e14f1a99c7d601a8b0ee
Sahib.Aujla <sahibpreet568@gmail.com>
2024-11-05 10:18:33 -0500

    added bencode

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const fs=require('fs');
 const path=require('path');
-const torrent=fs.readFileSync(path.join(__dirname,"puppy.torrent"));
+const bencode=require('bencode');
+const torrent=bencode.decode( fs.readFileSync(path.join(__dirname,"puppy.torrent")));
+console.log(torrent.announce.toString());
 console.log(torrent);
+
