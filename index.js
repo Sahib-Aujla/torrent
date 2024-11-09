809ab404c685a72ca5794750bb520405ba8bbda5
Sahib.Aujla <sahibpreet568@gmail.com>
2024-11-09 15:51:00 -0500

    updated indexjs

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const fs=require('fs');
-const bencode=require('bencode');
-const tracker=require('./src/tracker');
+const tracker = require("./tracker");
+const torrentParser = require("./torrent-parser");
 
+const torrent = torrentParser.open("puppy.torrent");
 
-const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
-
-tracker.getPeers(torrent, peers => {
-  console.log('list of peers: ', peers);
+tracker.getPeers(torrent, (peers) => {
+  console.log("list of peers: ", peers);
 });
