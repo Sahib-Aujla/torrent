d70dac331c7d1d032dd5662fa431466de1c736d4
Sahib.Aujla <sahibpreet568@gmail.com>
2024-11-09 16:11:30 -0500

    completed tracker js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const tracker = require("./tracker");
-const torrentParser = require("./torrent-parser");
+const tracker = require("./src/tracker");
+const torrentParser = require("./src/torrent-parser");
 
 const torrent = torrentParser.open("puppy.torrent");
-
+console.log(torrent);
 tracker.getPeers(torrent, (peers) => {
   console.log("list of peers: ", peers);
 });
--- src/torrent-parser.js
+++ src/torrent-parser.js
@@ -1,16 +1,26 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const bencode = require('bencode');
-
+const fs = require("fs");
+const bencode = require("bencode");
+const crypto = require("crypto");
+const bignum = require("bignumber.js");
 module.exports.open = (filepath) => {
   return bencode.decode(fs.readFileSync(filepath));
 };
 
-module.exports.size = torrent => {
-  // ...
+module.exports.size = (torrent) => {
+  const size = torrent.info.files
+    ? torrent.info.files.map((file) => file.length).reduce((a, b) => a + b)
+    : torrent.info.length;
+
+  const bigSize = new bignum(size);
+
+  // Convert BigNumber to a hexadecimal string, pad to 16 chars for 8 bytes, then create a buffer
+  const hexString = bigSize.toString(16).padStart(16, "0");
+  return Buffer.from(hexString, "hex");
 };
 
-module.exports.infoHash = torrent => {
-  // ...
+module.exports.infoHash = (torrent) => {
+  const info = bencode.encode(torrent.info);
+  return crypto.createHash("sha1").update(info).digest();
 };
