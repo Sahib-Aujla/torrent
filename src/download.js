b73bbd31c0c1e83637aed1bcd70b41b0454947f8
Sahib.Aujla <sahibpreet568@gmail.com>
2024-11-13 08:48:03 -0500

    added whole message

--- src/download.js
+++ src/download.js
@@ -1,6 +1,6 @@
 const net = require("net");
 const tracker = require("./tracker");
-
+const message = require('./message');
 module.exports = (torrent) => {
   tracker.getPeers(torrent, (peers) => {
     console.log("here");
@@ -13,5 +13,24 @@ const download = (peer) => {
   const socket = net.Socket();
   socket.on("error", console.log);
   socket.connect(peer.port, peer.ip, () => {});
-  socket.on("data", (data) => {});
+  onWholeMsg(socket, (data) => {
+    //handle the complete data here
+  });
+};
+
+const onWholeMsg = (socket, callback) => {
+  let savedBuf = Buffer.alloc(0);
+  let handshake = true;
+  socket.on("data", (recBuf) => {
+    const msgLen = () =>
+      handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readUInt32BE(0) + 4;
+
+    savedBuf = Buffer.concat([savedBuf, recBuf]);
+
+    while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+      callback(savedBuf.slice(0, msgLen()));
+      savedBuf = savedBuf.slice(msgLen());
+      handshake = false;
+    }
+  });
 };
