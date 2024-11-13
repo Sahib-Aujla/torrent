53a243269e7771f027b4854c4a043c9c1fc9b630
Sahib.Aujla <sahibpreet568@gmail.com>
2024-11-13 08:32:41 -0500

    added peer handling

--- src/download.js
+++ src/download.js
@@ -2,11 +2,16 @@ const net = require("net");
 const tracker = require("./tracker");
 
 module.exports = (torrent) => {
-  let Peers = [];
   tracker.getPeers(torrent, (peers) => {
     console.log("here");
     console.log("list of peers: ", peers);
-    if (peers) peers.forEach((p) => Peers.push(p));
-    console.log(Peers);
+    if (peers) peers.forEach(download);
   });
+};
+
+const download = (peer) => {
+  const socket = net.Socket();
+  socket.on("error", console.log);
+  socket.connect(peer.port, peer.ip, () => {});
+  socket.on("data", (data) => {});
 };
