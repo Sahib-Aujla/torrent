5b3962f13945c4e40624eb370196e382714434ef
Sahib.Aujla <sahibpreet568@gmail.com>
2024-11-14 08:20:34 -0500

    added download logic

--- src/download.js
+++ src/download.js
@@ -1,20 +1,34 @@
 const net = require("net");
 const tracker = require("./tracker");
-const message = require('./message');
+const message = require("./message");
 module.exports = (torrent) => {
   tracker.getPeers(torrent, (peers) => {
     console.log("here");
     console.log("list of peers: ", peers);
-    if (peers) peers.forEach(download);
+    if (peers) peers.forEach((peer) => download(peer, torrent));
   });
 };
 
-const download = (peer) => {
+const download = (peer, torrent) => {
   const socket = net.Socket();
-  socket.on("error", console.log);
-  socket.connect(peer.port, peer.ip, () => {});
-  onWholeMsg(socket, (data) => {
+  socket.on("error", (err) => {
+    if (err.code === "ECONNREFUSED") {
+      console.log(`Connection refused by peer ${peer.ip}:${peer.port}`);
+    } else if (err.code === "ETIMEDOUT") {
+      console.log(`Connection to peer ${peer.ip}:${peer.port} timed out`);
+    } else if (err.code === "ENOTFOUND") {
+      console.log(`Peer ${peer.ip}:${peer.port} not found`);
+    } else {
+      console.log(`Socket error on peer ${peer.ip}:${peer.port}:`, err);
+    }
+  });
+  socket.connect(peer.port, peer.ip, () => {
+    console.log("here66");
+    socket.write(message.buildHandshake(torrent));
+  });
+  onWholeMsg(socket, (msg) => {
     //handle the complete data here
+    msgHandler(msg, socket);
   });
 };
 
@@ -33,4 +47,20 @@ const onWholeMsg = (socket, callback) => {
       handshake = false;
     }
   });
+};
+
+const msgHandler = (msg, socket) => {
+  if (isHandshake(msg)) {
+    console.log("Handshake received, sending interested message");
+
+    socket.write(message.buildInterested());
+  }
+};
+
+const isHandshake = (msg) => {
+  console.log("lol");
+  return (
+    msg.length == msg.readUInt8(0) + 49 &&
+    msg.toString("utf8", 1) == "BitTorrent protocol"
+  );
 };
--- src/message.js
+++ src/message.js
@@ -1,17 +1,20 @@
 "use strict";
 const torrentParser = require("./torrent-parser");
-
+const utils = require("./utils");
 module.exports.buildHandshake = (torrent) => {
-  const buffer = Buffer.alloc(68);
-  buffer.writeUInt8(19, 0);
-  buffer.write("BitTorrent protocol", 1);
-  buffer.writeUInt32BE(0, 20);
-  buffer.writeUInt32BE(0, 24);
+  const buf = Buffer.alloc(68);
+  // pstrlen
+  buf.writeUInt8(19, 0);
+  // pstr
+  buf.write("BitTorrent protocol", 1);
+  // reserved
+  buf.writeUInt32BE(0, 20);
+  buf.writeUInt32BE(0, 24);
   // info hash
-  torrentParser.infoHash(torrent).copy(buffer, 28);
+  torrentParser.infoHash(torrent).copy(buf, 28);
   // peer id
-  buffer.write(util.genId());
-  return buffer;
+  buf.write(utils.genId());
+  return buf;
 };
 
 module.exports.buildKeepAlive = () => Buffer.alloc(4);
